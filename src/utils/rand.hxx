441d5e1a543556cdf76728512c757a1db304689b
lorewired <lorecode12@gmail.com>
2026-01-01 23:46:03 +0100

    fix: give each thread its own generator

--- src/utils/rand.hxx
+++ src/utils/rand.hxx
@@ -1,17 +1,20 @@
 #pragma once
 
-#include "data-types.hxx"
-
 #include <random>
 
-static std::default_random_engine generator;
 
-I32 r32ir(const I32 l, const I32 r) {
-    std::uniform_int_distribution<I32> distribution(l, r);
-    return distribution(generator);
+inline std::mt19937& GetRandomGenerator() {
+    static std::random_device rd; 
+    static std::mt19937 gen(rd()); 
+    return gen;
 }
 
-I64 r64ir(const I64 l, const I64 r) {
-    std::uniform_int_distribution<I64> distribution(l, r);
-    return distribution(generator);
+inline int r32ir(const int l, const int r) {
+    std::uniform_int_distribution<int> distribution(l, r);
+    return distribution(GetRandomGenerator());
+}
+
+inline long long r64ir(const long long l, const long long r) {
+    std::uniform_int_distribution<long long> distribution(l, r);
+    return distribution(GetRandomGenerator());
 }
