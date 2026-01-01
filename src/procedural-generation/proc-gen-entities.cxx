374ae894570d636c7cc23263a05fa8f0f2167ed6
lorewired <lorecode12@gmail.com>
2026-01-01 23:26:48 +0100

    refactor: renaming entities properties

--- src/procedural-generation/proc-gen-entities.cxx
+++ src/procedural-generation/proc-gen-entities.cxx
@@ -1,39 +1,59 @@
 #include "proc-gen.hxx"
 
-using namespace proc_gen;
+using namespace ProceduralGeneration;
 
 #pragma region Cell_Area
 
-cell::cell(const char __rep)
-{
-    switch (__rep) {
-        case '.': _type = cell_type::EMPTY; break;
-        case 'B': _type = cell_type::CHEST; break;
-    }
-}
-
-cell_type cell::type() const { return _type; }
-void cell::set_type(const cell_type __type) { _type = __type; }
+char Cell::Rep() const { return rep; }
+CellType Cell::Type() const { return type; }
+void Cell::SetType(CellType _type) { type = _type; rep = Glyph[_type]; }
 
 #pragma endregion
 
 #pragma region Map_Node_Area
-map_node::map_node(const map __map) : _map(__map) {}
-map_node ** map_node::adjacents() { return _adjacents; }
-map_node * map_node::get_top() { return _adjacents[0]; }
-map_node * map_node::get_right() { return _adjacents[1]; }
-map_node * map_node::get_down() { return _adjacents[2]; }
-map_node * map_node::get_left() { return _adjacents[3]; }
+
+MapNode::MapNode(Map _map) : map(_map), number(++ID) {}
+
+Map& MapNode::GetMap() { return map; }
+
+int MapNode::Number() const { return number; }
+
+bool ValidDirection(NodeDir dir) { return dir >= 0 && dir <= 3; }
+
+void MapNode::SetAdjacent(NodeDir dir, std::shared_ptr<MapNode> node) {
+    if (!ValidDirection(dir)) return;
+    adjacents[dir] = node;
+}
+
+std::shared_ptr<MapNode> MapNode::GetAdjacent(NodeDir dir) {
+    return ValidDirection(dir) ? adjacents[dir] : nullptr;
+}
+
+NodeDir MapNode::RandomNodeDirection() {
+    int randomDir = r32ir(0, 3);
+    switch (randomDir) {
+        case 0: return TOP;
+        case 1: return RIGHT;
+        case 2: return DOWN;
+        case 3: return LEFT;
+    }
+}
+
 #pragma endregion
 
 #pragma region Map_Area
 
-map::map(const UI32 __width, const UI32 __height)
-: _width(__width), _height(__height)
+Map::Map(int _width, int _height)
+: width(_width), height(_height)
 {
-    _matrix.assign(_width, std::vector<cell>(_height));
+    grid.resize(width * height);
 }
 
-std::vector<std::vector<cell>> map::matrix() const { return _matrix; }
+int Map::Width() const { return width; }
+int Map::Height() const { return height; }
+
+Cell& Map::At(int x, int y) { return grid[x * width + y]; }
+
+const std::vector<Cell>& Map::Data() const { return grid; }
 
 #pragma endregion
