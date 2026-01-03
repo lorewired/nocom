c06d0bf888ec44d052860f0f6b556910e8586590
lorewired <lorecode12@gmail.com>
2026-01-03 18:17:41 +0100

    feat: terminal functions

---
+++ src/core/terminal.hh
@@ -0,0 +1,11 @@
+#pragma once
+#include <windows.h>
+#include <iostream>
+
+namespace Game::Terminal {
+    extern HANDLE hConsole;
+
+    void HideCursor();
+    void PositionCursor(int x, int y);
+    void CleanScreen();
+}
