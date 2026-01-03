8e70009f67b0133e8e795375a3a6746994fd3fa5
lorewired <lorecode12@gmail.com>
2026-01-03 18:10:47 +0100

    feat: add map display function

--- src/core/window.cc
+++ src/core/window.cc
@@ -3,6 +3,29 @@
 Game::Core::Window::Window(std::shared_ptr<Game::ProceduralGeneration::MapNode> _initialRoom)
 : initialRoom(_initialRoom) {}
 
-void Game::Core::Window::Display(std::shared_ptr<Game::ProceduralGeneration::MapNode> node) {
-    
+void Game::Core::Window::Render(std::shared_ptr<Game::ProceduralGeneration::MapNode> room) {
+    Game::Terminal::PositionCursor(0, 0);
+
+    Game::Entities::Map& map = room->GetMap();
+
+    for (int y = 0; y < map.Height(); y++) {
+        for (int x = 0; x < map.Width(); x++) {
+            std::cout << map.At(x, y).Rep();
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << '\n';
+    std::cout.flush();
+}
+
+void Game::Core::Window::RenderPlayer(const Game::Entities::Human& player) {
+    // TODO: move cursor and print player.
+    Game::Terminal::PositionCursor(player.X(), player.Y());
+    std::cout << "O";
+    std::cout.flush();
+}
+
+void Game::Core::Window::FinishRender(const std::shared_ptr<Game::ProceduralGeneration::MapNode>& room) {
+    Game::Terminal::PositionCursor(0, room->GetMap().Height() + 1);
 }
--- src/core/window.hh
+++ src/core/window.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "src/procedural-generation/proc-gen.hh"
+#include "src/entities/human.hh"
+#include "terminal.hh"
 
 #include <memory>
 
@@ -13,7 +15,10 @@ namespace Game::Core {
         Window() = default;
         Window(std::shared_ptr<Game::ProceduralGeneration::MapNode> _initialRoom);
 
-        void Display(std::shared_ptr<Game::ProceduralGeneration::MapNode> node);
-    };
+        void Render(std::shared_ptr<Game::ProceduralGeneration::MapNode> room);
+        void RenderPlayer(const Game::Entities::Human& player);
+        void FinishRender(const std::shared_ptr<Game::ProceduralGeneration::MapNode>& room);
+        // void RenderEnemy(const Game::Entities::Enemy& enemy);
+};
 
 }
